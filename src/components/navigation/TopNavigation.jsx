c05469a51d9180cff6fa5d05b3e17c6a253677df
mathopoulos <alexandros@mathopoulos.com>
2023-07-24 23:06:09 +0000

    Added comments to TopNavigation.jsx.

--- src/components/navigation/TopNavigation.jsx
+++ src/components/navigation/TopNavigation.jsx
@@ -1,11 +1,15 @@
+// Import necessary modules and components
 import React from 'react';
 import '/src/App.css';
 import RightTopNavigation from './RightTopNavigation';
 import AppNavigation from './AppNavigation';
 
+// Define the TopNavigation functional component
 export default function TopNavigation(props) {
+  // Destructure the shapes prop
   const { shapes } = props;
-  
+
+  // Render the component
   return (
     <div className= "topDiv">
       <AppNavigation shapes={shapes}/>
