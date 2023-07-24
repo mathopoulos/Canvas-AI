91816fa1314bf19d589bbb1e0978d9b35de2114f
mathopoulos <alexandros@mathopoulos.com>
2023-07-24 22:56:26 +0000

    Added comments to index.jsx file.

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,13 @@
+// Import necessary modules from React and ReactDOM libraries
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+// Import the main App component
 import App from './App'
 
+// Create a root DOM node for React and render the App component inside it
+// Using React's Concurrent Mode with createRoot
+// React.StrictMode is a wrapper to highlight potential problems in the app during development
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<App />
