0751e84f18a1b91cd59b828defeedc3edc59a1fa
mathopoulos <alexandros@mathopoulos.com>
2023-07-24 23:47:38 +0000

    Added comments to useCanvasUpdate.jsx.

--- src/components/canvas/useCanvasUpdate.jsx
+++ src/components/canvas/useCanvasUpdate.jsx
@@ -2,24 +2,32 @@ import { useEffect } from 'react';
 import { drawShapes } from '/src/components/drawingComponents/shapeDrawing.jsx';
 import { updateResizingBox } from '/src/components/helpers.jsx';
 
+// Custom hook to handle canvas updates
 export const useCanvasUpdate = (canvasRef, resizingBoxRef, shapes, selectedShapeIndex) => {
   useEffect(() => {
+    // Get the canvas and its 2D context
     const canvas = canvasRef.current;
     const context = canvas.getContext('2d');
 
+    // Get the device pixel ratio, fall back to 1 if not available
     const dpr = window.devicePixelRatio || 1;
 
+    // Adjust canvas dimensions based on device pixel ratio
     canvas.width = 310 * dpr;
     canvas.height = 100 * dpr;
     canvas.style.width = '310px';
     canvas.style.height = '100px';
-    
+
+    // Clear the canvas
     context.clearRect(0, 0, canvas.width, canvas.height);
 
+    // Adjust the scale of the context based on device pixel ratio
     context.scale(dpr, dpr);
 
+    // Draw the shapes on the canvas
     drawShapes(context, shapes, selectedShapeIndex); // pass selectedShapeIndex to drawShapes
 
+    // Update the resizing box based on the selected shape
     updateResizingBox(resizingBoxRef, selectedShapeIndex, shapes);
   }, [canvasRef, resizingBoxRef, shapes, selectedShapeIndex]);
 };
