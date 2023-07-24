b2301358fcb5f47eb092c20c03bd2816f5ffee75
mathopoulos <alexandros@mathopoulos.com>
2023-07-24 23:04:12 +0000

    Added comments to RightTopNavigation.jsx

--- src/components/navigation/RightTopNavigation.jsx
+++ src/components/navigation/RightTopNavigation.jsx
@@ -1,8 +1,11 @@
+// Import necessary modules and components
 import React from 'react';
 import '/src/App.css';
 
+// Define the RightTopNavigation functional component
 export default function RightTopNavigation() {
-  
+
+  // Render the component
   return (
       <div id="rightTopNav">  
       <img id="profile" src="images/profile.svg" alt="box-icon" />
