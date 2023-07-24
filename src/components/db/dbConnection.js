c59837ca78aa1601026f5221764ee505cddb9c89
mathopoulos <alexandros@mathopoulos.com>
2023-07-24 23:16:03 +0000

    Added comments to dbConnection.js

--- src/components/db/dbConnection.js
+++ src/components/db/dbConnection.js
@@ -1,7 +1,10 @@
+// Importing the required module from 'pg' package
 import pg from 'pg';
 
+// Destructuring the Pool class from 'pg' for creating a connection pool
 const { Pool } = pg;
 
+// Setting up the configuration for the PostgreSQL connection pool
 const pool = new Pool({
   user: 'neon',
   host: process.env['PGHOST'],
@@ -13,4 +16,5 @@ const pool = new Pool({
   },
 });
 
+// Exporting the pool so it can be used elsewhere in the application
 export default pool;
