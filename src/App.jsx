c26b467992b20bd23a331cf06cff0da83ac66473
mathopoulos <alexandros@mathopoulos.com>
2023-07-24 22:54:51 +0000

    Added comments to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
+// Importing required styles for the App component
 import './App.css'
+
+// Importing necessary React hooks and components
 import React, { useEffect, useState } from 'react';
 import TopNavigation from '/src/components/navigation/TopNavigation.jsx';
 import Canvas from '/src/components/canvas/Canvas.jsx';
 
 
-
+// Main App component
 export default function App() {
+  // State to hold text fetched from the API
   const [text, setText] = useState('');
 
+  // useEffect hook to fetch data from the API when the component mounts
   useEffect(() => {
+    // Fetching data from the '/api' endpoint
     fetch('/api')
-      .then(res => res.text())
-      .then(setText);
-  }, []);
+      .then(res => res.text()) // Parsing the response to text
+      .then(setText); // Setting the fetched text to the state
+  }, []); // Empty dependency array ensures this useEffect runs only once, similar to componentDidMount
 
   return (
     <main>
