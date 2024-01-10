bc7d743ce36c1ff006aab0d6cd99b83f4be0120b
mathopoulos <alexandros@mathopoulos.com>
2024-01-10 03:13:52 +0000

    Improved component dropdown ux

--- src/components/navigation/Switcher.jsx
+++ src/components/navigation/Switcher.jsx
@@ -3,13 +3,17 @@ import '/src/components/navigation/Switcher.css';
 
 
 export default function Switcher(props) {
-  // Destructure shapes from props
-  const {selected} = props;
+  const [showDropdown, setShowDropdown] = useState(false);
+  const items = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5'];
 
-    // State to track the button click
+  const handleComponentSwitcherClick = () => {
+    setShowDropdown(!showDropdown);
+  }
+
+  // State to track the button click
   const [isDoodleButtonClicked, setDoodleButtonClicked] = useState(false);
 
-    // Function to handle button click
+  // Function to handle button click
   const handleDoodleButtonClick = () => {
     setDoodleButtonClicked(true);
   }
@@ -17,19 +21,37 @@ export default function Switcher(props) {
 
   // Render the component
   return (
-    <div className = "navigation">
+    <div className="navigation">
       <div className="advancedMenuNavigation">
-        <img className ="advancedMenuNavigationIcon" src="/images/view-list.svg" alt="box-icon" style={{ userSelect: 'none' }}  />
-      <div className ="componentSwitcher"> First Name <img className ="downIcon" src="/images/down.svg" alt="box-icon" /> </div>
+        <img className="advancedMenuNavigationIcon" src="/images/view-list.svg" alt="box-icon" style={{ userSelect: 'none' }} />
+        <div className="componentSwitcher" onClick={handleComponentSwitcherClick}> First Name <img className="downIcon" src="/images/down.svg" alt="box-icon" /> </div>
+        {showDropdown && (
+          <div className="dropdown-menu">
+            <div className="dropdown-menu-header">
+            <div className="dropdown-menu-content">
+              Components
+            </div>
+            <img className="plusIcon" src="/images/plus.svg" alt="box-icon" /> </div>
+            <div className="layersLine"></div>
+            {items.map((item, index) => (
+              <div className="dropdown-menu-items" key={index} style={{ display: 'flex', alignItems: 'center' }}>
+                <img
+                  src="images/layerIcon.svg"
+                  id="#dragIcon"
+                  style={{ marginRight: '5px' }}
+                />{item}</div>
+            ))}
+          </div>
+        )}
       </div>
       <div className="modeSwitcher">
-        <div className={isDoodleButtonClicked ? 'doodle-button-clicked' : 'doodle-button'} 
+        <div className={isDoodleButtonClicked ? 'doodle-button-clicked' : 'doodle-button'}
           onClick={handleDoodleButtonClick}>Doodle</div>
         <div className="high-fidelity-button">High Fidelity</div>
         <div className="functional-button">Functional</div>
       </div>
-      </div>
-    )
+    </div>
+  )
 }
 
 
